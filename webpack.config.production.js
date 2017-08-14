383fb1d9e5c8e9ca9dfb3a2345861a3355264516
liyang <liyang@bonc.com.cn>
2017-08-14 11:06:29 +0800

    添加webpack的插件uglify进行文件压缩

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,10 +3,12 @@ const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 // css分离
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Es3ifyPlugin = require('es3ify-webpack-plugin');
 
 module.exports = {
   entry: {
     entry: './src/entry.js',
+    /*vendor: ['console-polyfill', 'es5-shim', 'es5-shim/es5-sham', 'es6-promise', 'fetch-ie8', 'es3ify-loader', 'react', 'react-dom', 'react-router'],*/
   },
   output: {
     path: path.join(__dirname, 'build'),
@@ -15,12 +17,18 @@ module.exports = {
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
-
-    // 压缩js文件，ie8支持有问题
+    // 压缩js文件，ie8支持使用Es3ifyPlugin
     new webpack.optimize.UglifyJsPlugin({
-      mangle: false,
+      mangle: {
+        screw_ie8: false,
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: false,
+      },
+      comments: false, // 去除注释
     }),
-
+    new Es3ifyPlugin(),
     // 定义全局环境变量为生产环境
     new webpack.DefinePlugin({
       'process.env': {
@@ -34,6 +42,7 @@ module.exports = {
       },
     }),
     new ExtractTextPlugin('css.all.css'),
+    /*new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),*/
   ],
   resolve: {
     // 实际就是自动添加后缀，默认是当成js文件来查找路径
@@ -48,6 +57,7 @@ module.exports = {
       // 现在你可以这样引用 import { Nav } from 'app/containers'
       actions: path.resolve(__dirname, 'src/js/actions'),
       images: path.resolve(__dirname, 'src/images'),
+      store: path.resolve(__dirname, 'src/js/store'),
 
       // 注意：别名只能在.js文件中使用。
     },
@@ -63,7 +73,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader'],
+        loaders: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /(fontawesome-webfont|glyphicons-halflings-regular)\.(woff|woff2|ttf|eot|svg)($|\?)/,
